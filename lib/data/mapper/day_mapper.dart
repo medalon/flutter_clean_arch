cc8bf4e69250a14869a44989c761025f5273e760
medalon <medalon07@gmail.com>
2022-05-04 12:24:59 +0600

    added mapper

---
+++ lib/data/mapper/day_mapper.dart
@@ -0,0 +1,13 @@
+import 'package:my_clean_arch/data/api/api_day.dart';
+import 'package:my_clean_arch/domain/model/day.dart';
+
+class DayMapper {
+  static Day fromApi(ApiDay day) {
+    return Day(
+      sunrise: DateTime.tryParse(day.sunrise) as DateTime,
+      sunset: DateTime.tryParse(day.sunset) as DateTime,
+      solarNoon: DateTime.tryParse(day.solarNoon) as DateTime,
+      dayLength: day.dayLength.toInt(),
+    );
+  }
+}
