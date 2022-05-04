9041d3cfdb398d1104bc0f85d0b1860f267f58dc
medalon <medalon07@gmail.com>
2022-05-04 12:06:18 +0600

    added folders model and repository

---
+++ lib/domain/repository/day_repository.dart
@@ -0,0 +1,8 @@
+import 'package:my_clean_arch/domain/model/day.dart';
+
+abstract class DayRepository {
+  Future<Day> getDay({
+    required double latitude,
+    required double longitude,
+  });
+}
