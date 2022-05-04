e81f4e924cbef1a19ab3905df9c192f37fb48363
medalon <medalon07@gmail.com>
2022-05-04 11:32:32 +0600

    added folders for clean arch

---
+++ lib/internal/application.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:my_clean_arch/presentation/home.dart';
+
+class Application extends StatelessWidget {
+  const Application({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "My Demo Clean Arch",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const Home(),
+    );
+  }
+}
